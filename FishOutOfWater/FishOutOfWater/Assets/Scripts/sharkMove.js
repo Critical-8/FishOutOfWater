20090d5c85c2d552dce524851f8cdfe31f4382d0
Michael Tang <mtang8264@gmail.com>
2017-06-11 10:33:19 -0400

    Added Shark and Animation

---
+++ FishOutOfWater/FishOutOfWater/Assets/Scripts/sharkMove.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+//variables
+public var timer = 0;
+public var speed = 2.5;
+
+public var fish : GameObject;
+public var shark : GameObject;
+
+function Start () {
+	
+}
+
+function Update () {
+	//move
+	shark.transform.position.x += speed / 60;
+
+	//update speed
+	if(timer >= 120 && shark.transform.position.x >= fish.transform.position.x - 4){
+		speed = 2;
+	}
+	if(shark.transform.position.x < fish.transform.position.x - 4){
+		speed = 2.5;
+	}
+
+	//update timer
+	timer ++;
+}
